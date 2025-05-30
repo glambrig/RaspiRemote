62cb964db54e96bed0ddf97e6f8553924d6f4e70
glambrig <glambrig@student.42berlin.de>
2025-05-30 15:42:34 +0200

    Changed build script

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ if [ "$1" != "-noapt" ] || [ "$2" != "-noapt" ] || [ "$3" != "-noapt" ]; then
 fi
 
 if [ ! -d "inc/LibUinputWrapper" ]; then
+	echo "LibUinputWrapper not found, installing..."
 	git clone git@github.com:glambrig/LibUinputWrapper.git
 	mv LibUinputWrapper inc
 	make -C inc/LibUinputWrapper
@@ -14,7 +15,7 @@ fi
 
 if [ "$1" = "-nopi" ] || [ "$2" = "-nopi" ] || [ "$3" = "-nopi" ]; then
 	if [ ! -d "inc/WiringPi" ] && ! gpio -v >/dev/null 2>&1; then
-	git clone git@github.com:WiringPi/WiringPi.git
+		git clone git@github.com:WiringPi/WiringPi.git
 		cd WiringPi
 		./build debian
 		mv debian-template/wiringpi_3.14_amd64.deb .
@@ -27,9 +28,10 @@ if [ "$1" = "-nopi" ] || [ "$2" = "-nopi" ] || [ "$3" = "-nopi" ]; then
 	fi
 else
 	if [ ! -d "inc/WiringPi" ] && ! command -v gpio &> /dev/null ; then
-		git clone git@github.com:WiringPi/WiringPi.git
+		echo "WiringPi not found, installing..."
+		git clone git@github.com:WiringPi/WiringPi.git || exit 1
 		cd WiringPi
-		./build debian
+		./build debian || exit 1
 		mv debian-template/wiringpi_3.14_arm64.deb .
 		sudo chmod 644 wiringpi_3.14_arm64.deb
 		sudo apt install ./wiringpi_3.14_arm64.deb
@@ -38,8 +40,9 @@ else
 	else
 		echo "WiringPi already installed, skipping..."
 	fi
-
 fi
+
+echo "WiringPi installed successfully."
 
 if [ "$1" != "-nocopy" ] && [ "$2" != "-nocopy" ] && [ "$3" != "-nocopy" ]; then
 	sudo cp conf/lircd.conf /etc/lirc/lircd.conf
@@ -47,6 +50,8 @@ if [ "$1" != "-nocopy" ] && [ "$2" != "-nocopy" ] && [ "$3" != "-nocopy" ]; then
 	sudo cp conf/lirc_options.conf /etc/lirc/lirc_options.conf
 	# rm lircd.conf lircrc lirc_options.conf
 fi
+
+echo "Changing permissions for /dev/uinput ..."
 
 sudo modprobe uinput
 sudo chmod 777 /dev/uinput
